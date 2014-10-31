a02be0fc109c976fe7e5b9402e011187440a5dee
Darrel <darrel@tavis.ca>
2014-10-30 22:53:05 -0400

    Added Echo controller for testing

--- RunscopeSlackApi/ActionController.cs
+++ RunscopeSlackApi/ActionController.cs
@@ -13,6 +13,18 @@ namespace RunscopeSlackApi
         {
             var text = formData["text"];
 
+            // Runscope: Run HttpCheck with x=y
+            // Split off Runscope,
+            //var commandText = text.ToLowerInvariant().Tr.Remove("")
+            // get Run verb
+            
+
+            // Convert bucket name into bucket key
+            // Convert get bucket radar info from API to get trigger URL
+            // Add parameters to the end of the trigger URL
+
+            // Make the request
+
             return new ResponseMessageResult(new HttpResponseMessage()
             {
                 Content = new StringContent("{ \"text\" : \"action completed : " + text + " \" }", Encoding.UTF8,"application/json")
---
+++ RunscopeSlackApi/EchoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RunscopeSlackApi
+{
+    public class EchoController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post()
+        {
+            await Request.Content.LoadIntoBufferAsync();
+
+            return new HttpResponseMessage() {Content = Request.Content};
+        }
+    }
+}
